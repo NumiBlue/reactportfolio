79f5143c422e5ef6396ee70eef7c8b0e64bd0072
NumiBlue <thekindlyblog@gmail.com>
2022-06-09 00:49:15 -0400

    tracking navbar

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,10 +1,11 @@
 import './App.css';
-import navbar from './components/navbar.jsx.'
+import Navbar from './components/navbar';
+
+
 function App() {
   return (
     <div>
-      <navbar/>
-      
+    <Navbar/>
     </div>
   );
 }

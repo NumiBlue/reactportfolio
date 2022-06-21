845e43bfccfa4ee78aa30ba6b177215af7845f11
NumiBlue <thekindlyblog@gmail.com>
2022-06-20 22:44:09 -0400

    saves intro.jsx

--- my-app/src/components/intro/Intro.jsx
+++ my-app/src/components/intro/Intro.jsx
@@ -1,9 +1,21 @@
-import "./intro.css";
+import "./intro/Intro.css";
 
 const Intro = () => {
     return (
         <div classname = "n">
-            <n className = "n-left">left</n>
+            <n className = "n-left">
+                <div className="n-left-wrapper">
+                    <h2>
+                        <div className="n-intro">
+                        Hi, I'm 
+                        </div>
+                        <div className="n-name">
+                        <h1>Ciara Hargis.
+                          </h1>
+                          </div>
+                    </h2>
+                </div>
+            </n>
             <n classname = "n-right">right</n>
         </div>
     )

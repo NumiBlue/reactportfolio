a5d243bf32f58d942b3f0f56ed47b59b52c1daef
NumiBlue <thekindlyblog@gmail.com>
2022-06-21 21:54:20 -0400

    adds background image to hero

--- my-app/src/components/Home.jsx
+++ my-app/src/components/Home.jsx
@@ -1,8 +1,12 @@
-import React from 'react'
+import React from 'react';
+import backgroundImage from '../assets/wes-hicks-G2W_8eagxNQ-unsplash-2.jpg';
 
 const Home = () => {
   return (
-    <div>
+    <div name='home' className='w-full h-screen'>
+    {/* Container */}
+    <div style={{backgroundImage: `url(${backgroundImage})`}}
+    className='bg-cover flex flex-col justify-center text-left h-full'></div>
       </div>
 )
 }
--- my-app/src/components/Navbar.jsx
+++ my-app/src/components/Navbar.jsx
@@ -1,10 +1,13 @@
 import React from 'react';
-
+// import Logo from '../assets/wes-hicks-G2W_8eagxNQ-unsplash-2.jpg';
 import { FaBars, FaTimes } from 'react-icons/fa';
 
 const Navbar = () => {
   return (
     <div className='fixed w-full h-[80px] flex justify-between items-center px-4 bg-gradient-to-r from-indigo-800 via bg-red-800 to bg-yellow-600 text-gray-300'>
+      {/* <div>
+        <img src={Logo} alt="Logo" style={{width: '350px'}}/>
+        </div> */}
 
       {/* menu */}
     <div>

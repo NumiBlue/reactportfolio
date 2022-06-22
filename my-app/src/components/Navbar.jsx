5107ff3519e0d710097e0da2f9179bf730caa263
NumiBlue <thekindlyblog@gmail.com>
2022-06-21 21:44:48 -0400

    navbar

--- my-app/src/components/Navbar.jsx
+++ my-app/src/components/Navbar.jsx
@@ -1,13 +1,37 @@
-import React from 'react'
+import React from 'react';
 
-const navbar = () => {
+import { FaBars, FaTimes } from 'react-icons/fa';
+
+const Navbar = () => {
   return (
     <div className='fixed w-full h-[80px] flex justify-between items-center px-4 bg-gradient-to-r from-indigo-800 via bg-red-800 to bg-yellow-600 text-gray-300'>
-    <div>
-      Hi
-    </div>
-    </div>
-  )
-}
 
-export default navbar
+      {/* menu */}
+    <div>
+      <ul className='flex'>
+        <li>Home</li>
+        <li>About</li>
+        <li>Portfolio</li>
+        <li>Resume</li>
+        <li>Contact</li>
+      </ul>
+    </div>
+
+    {/* hamburger */}
+    <div className='hidden'>
+      <FaBars />
+    </div>
+    {/* mobile menu */}
+    <ul className='hidden'>
+        <li>Home</li>
+        <li>About</li>
+        <li>Portfolio</li>
+        <li>Resume</li>
+        <li>Contact</li>
+      </ul>
+
+    </div>
+  );
+};
+
+export default Navbar; 

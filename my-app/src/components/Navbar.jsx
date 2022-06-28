5c27250d7c6da104b076d175f053d5c272b43cde
NumiBlue <thekindlyblog@gmail.com>
2022-06-28 14:28:14 -0400

    adds hamburger bg

--- my-app/src/components/Navbar.jsx
+++ my-app/src/components/Navbar.jsx
@@ -14,6 +14,8 @@ import { BsFillPersonLinesFill } from 'react-icons/bs';
 import { Link } from 'react-scroll';
 
 const Navbar = () => {
+  const [nav, setNav] = useState(false);
+  const handleClick = () => setNav(!nav);
   return (
     <div className='fixed w-full h-[80px] flex justify-between items-center px-4 bg-gradient-to-r from-indigo-800 via bg-red-800 to bg-yellow-600 text-gray-300'>
       {/* <div>
@@ -49,17 +51,47 @@ const Navbar = () => {
         </li>
       </ul>
 
-    {/* hamburger 
+    {/* hamburger */}
      <div onClick={handleClick} className='md:hidden z-10'>
         {!nav ? <FaBars /> : <FaTimes />}
-      </div> */}
+      </div> 
     {/* mobile menu */}
-    <ul className='hidden'>
-        <li>Home</li>
-        <li>About</li>
-        <li>Portfolio</li>
-        <li>Resume</li>
-        <li>Contact</li>
+    <ul
+        className={
+          !nav
+            ? 'hidden'
+            : 'absolute top-0 left-0 w-full h-screen bg-indigo-800 flex flex-col justify-center items-center'
+        }
+      >
+        <li className='py-6 text-4xl'>
+          <Link onClick={handleClick} to='home' smooth={true} duration={500}>
+            Home
+          </Link>
+        </li>
+        <li className='py-6 text-4xl'>
+          {' '}
+          <Link onClick={handleClick} to='about' smooth={true} duration={500}>
+            About
+          </Link>
+        </li>
+        <li className='py-6 text-4xl'>
+          {' '}
+          <Link onClick={handleClick} to='skills' smooth={true} duration={500}>
+            Skills
+          </Link>
+        </li>
+        <li className='py-6 text-4xl'>
+          {' '}
+          <Link onClick={handleClick} to='work' smooth={true} duration={500}>
+            Work
+          </Link>
+        </li>
+        <li className='py-6 text-4xl'>
+          {' '}
+          <Link onClick={handleClick} to='contact' smooth={true} duration={500}>
+            Contact
+          </Link>
+        </li>
       </ul>
      
      {/*social icons*/}

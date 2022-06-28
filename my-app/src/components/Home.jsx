0641a86093178d5fb4f1b0a8b1585719735b0acb
NumiBlue <thekindlyblog@gmail.com>
2022-06-28 15:05:49 -0400

    adds styling to work button

--- my-app/src/components/Home.jsx
+++ my-app/src/components/Home.jsx
@@ -21,9 +21,18 @@ const Home = () => {
         backSpeed={140} 
         loop
         />
-    </div>
+        <p className='pl-40 ml-8 px-8 text-black py-4 max-w-[500px]'> Hi! I'm a full-stack MERN developer and free-lance web developer. I'm focusing on perfecting my skills and expanding my clientele. </p>
+        <div>
+          <button className='text-black group px-6 py-3 my-2 flex items-center hover:bg-red-600 hover:border-red-600'>
+            View Work
+            <span className='group-hover:rotate-90 duration-300'>
+              <HiArrowNarrowRight className='ml-3 ' />
+            </span>
+          </button>
+        </div>
       </div>
-)
-}
+    </div>
+  );
+};
 
 export default Home 

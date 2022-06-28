04903abaac1c7afaac64d91eed2523e013d3f903
NumiBlue <thekindlyblog@gmail.com>
2022-06-28 15:20:02 -0400

    sets up about section

--- my-app/src/components/About.jsx
+++ my-app/src/components/About.jsx
@@ -6,13 +6,23 @@ const About = () => {
       <div className='flex flex-col justify-center items-center w-full h-full'>
         <div className='max-w-[1000px] w-full grid grid-cols-2 gap-8'>
           <div className='sm:text-right pb-8 pl-4'>
-            <p className='text-4xl font-bold inline border-b-4 border-pink-600'>
+            <p className='text-4xl font-bold inline border-b-4 border-red-600'>
               About
             </p>
     </div>
+    <div></div>
+    </div>
+    <div className='max-w-[1000px] w-full grid sm:grid-cols-2 gap-8 px-4'>
+            <div className='sm:text-right text-4xl font-bold'>
+              <p>Welcome to my Portfolio. I'm Ciara. Feel free to check out my work.</p>
+            </div>
+            <div>
+              <p>This is my first React Portfolio. After finishing a MERN-stack development course, I'm perfecting my React skills and working on fantastic web designs to improve the user experience. </p>  
+            </div>
     </div>
     </div>
     </div>
+    
   )
 }
 

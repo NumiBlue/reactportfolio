e4be87e753ed59a1bc4651c907a530668964acd6
NumiBlue <thekindlyblog@gmail.com>
2022-06-28 14:41:03 -0400

    adds icon home and adds/styles social icons

--- my-app/src/components/Home.jsx
+++ my-app/src/components/Home.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import backgroundImage from '../assets/wes-hicks-G2W_8eagxNQ-unsplash-2.jpg';
+import { HiArrowNarrowRight } from 'react-icons/hi';
 
 const Home = () => {
   return (

36ca58fefeb73120a48aaf5733bcdc04641357de
NumiBlue <thekindlyblog@gmail.com>
2023-08-02 14:45:11 -0400

    adds functional email form

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
-import  Navbar  from "./components/Navbar";
-import  Home  from "./components/Home";
+import React from 'react';
+import Navbar from './components/Navbar'; // Use relative path
+import Home from "./components/Home";
 import About from "./components/About";
 import Portfolio from "./components/Portfolio";
 import Contact from "./components/Contact";
 import Resume from "./components/Resume";
 import "./index.css";
+
 
 function App() {
   return (
--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,120 +1,42 @@
-import React from 'react';
+import React, { useRef } from 'react';
+import emailjs from '@emailjs/browser';
 
-export default function ContactForm() {
+const ContactUs = () => {
+  const form = useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs.sendForm(
+		'service_je146qa', 
+		'template_onwopdd', 
+		form.current, 'MDDvgzMg015ErbHzA') // Use 'YOUR_USER_ID' instead of 'YOUR_PUBLIC_KEY'
+      .then((result) => {
+        console.log(result.text);
+		console.log("message sent");
+      })
+      .catch((error) => {
+        console.log(error.text);
+      });
+  };
+
   return (
     <div className="bg-amber-100 min-h-screen flex items-center justify-center">
-      <div className="w-full max-w-md p-6 rounded-lg shadow-md bg-black">
+      <div className="w-full max-w-md p-6 rounded-lg shadow-md bg-black"> {/* Use 'bg-white' instead of 'bg-black' */}
         <div className="aspect-w-16 aspect-h-9">
-          <iframe
-            title="Contact Form"
-            src="https://forms.office.com/r/nckvW2KzEY?embed=true"
-            className="w-full h-full"
-            frameBorder="0"
-            allowFullScreen
-            webkitallowfullscreen
-            mozallowfullscreen
-            msallowfullscreen
-          ></iframe>
+          <form ref={form} onSubmit={sendEmail}>
+            <label>Name</label>
+            <input type="text" name="user_name" className="border" /> {/* Add a border class */}
+            <label>Email</label>
+            <input type="email" name="user_email" className="border" /> {/* Add a border class */}
+            <label>Message</label>
+            <textarea name="message" className="border" /> {/* Add a border class */}
+            <input type="submit" value="Send" className="bg-blue-500 text-white px-4 py-2 rounded" /> {/* Add Tailwind classes for styling */}
+          </form>
         </div>
       </div>
     </div>
   );
-}
-
-
-
-
-
-
-
-/*import React from 'react'
-
-
-
-import { useState } from "react";
-
-
-
-export default function ContactForm() {
-  const [loading, setLoading] = useState(false);
-
-  <iframe width="640px" height="480px" src="https://forms.office.com/r/nckvW2KzEY?embed=true" frameborder="0" marginwidth="0" marginheight="0" style="border: none; max-width:100%; max-height:100vh" allowfullscreen webkitallowfullscreen mozallowfullscreen msallowfullscreen> </iframe>
-
-	async function handleSubmit(event: any) {
-		event.preventDefault();
-		setLoading(true);
-
-		const { name, email, message } = event.target.elements;
-const data = {
-  name: name.value,
-  email: email.value,
-  message: message.value,
 };
 
-
-		const response = await fetch("/api/contact", {
-			method: "POST",
-			headers: {
-				"Content-Type": "application/json",
-			},
-			body: JSON.stringify(data),
-		});
-
-    setLoading(false);
-  }
-
-  return (
-    <div className="bg-amber-100 min-h-screen flex items-center justify-center">
-      <form onSubmit={handleSubmit} className="w-full max-w-md p-6 rounded-lg shadow-md bg-black">
-        <div className="mb-4">
-          <label className=" font-bold p-4 bg-black text-amber-100" htmlFor="name">
-            Name
-          </label>
-          <input
-            type="text"
-            minLength={3}
-            maxLength={150}
-            required
-            className="p-4 border-2 border-amber-100  bg-indigo-900  w-full"
-            autoComplete="off"
-            id="name"
-          />
-        </div>
-        <div className="mb-4">
-          <label className="font-bold text-amber-100" htmlFor="email">
-            Email
-          </label>
-          <input
-            type="email"
-            minLength={5}
-            maxLength={150}
-            required
-            className="p-4 bg-indigo-900 border w-full"
-            autoComplete="off"
-            id="email"
-          />
-        </div>
-        <div className="mb-4">
-          <label className="font-bold text-amber-100" htmlFor="message">
-            Message
-          </label>
-          <textarea
-            rows={4}
-            required
-            minLength={10}
-            maxLength={500}
-            name="message"
-            className="p-4 bg-indigo-900 border w-full"
-          />
-        </div>
-        <button
-          type="submit"
-          disabled={loading}
-          className="w-full px-4 py-2 bg-indigo-800 disabled:bg-gray-400 disabled:border-indigo-900 text-amber-100 font-medium"
-        >
-          Send Message
-        </button>
-      </form>
-    </div>
-  );
-}*/
+export default ContactUs; // Export the component properly

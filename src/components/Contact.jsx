23548a68e5330d762f03080d1048208ce7279014
NumiBlue <thekindlyblog@gmail.com>
2023-08-02 15:19:08 -0400

    styles functioning contact form

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -21,22 +21,45 @@ const ContactUs = () => {
   };
 
   return (
-    <div className="bg-amber-100 min-h-screen flex items-center justify-center">
-      <div className="w-full max-w-md p-6 rounded-lg shadow-md bg-black"> {/* Use 'bg-white' instead of 'bg-black' */}
-        <div className="aspect-w-16 aspect-h-9">
-          <form ref={form} onSubmit={sendEmail}>
-            <label>Name</label>
-            <input type="text" name="user_name" className="border" /> {/* Add a border class */}
-            <label>Email</label>
-            <input type="email" name="user_email" className="border" /> {/* Add a border class */}
-            <label>Message</label>
-            <textarea name="message" className="border" /> {/* Add a border class */}
-            <input type="submit" value="Send" className="bg-blue-500 text-white px-4 py-2 rounded" /> {/* Add Tailwind classes for styling */}
-          </form>
-        </div>
+  <div className="bg-amber-100 min-h-screen flex items-center justify-center">
+    <div className="w-full max-w-md p-6 rounded-lg shadow-md bg-black border border-gray-800 text-grey-800">
+      <div className="aspect-w-16 aspect-h-9">
+        <form ref={form} onSubmit={sendEmail} className="space-y-4">
+          <div className="flex flex-col items-center">
+            <label className="text-center">Name</label>
+            <input
+              type="text"
+              name="user_name"
+              className="bg-amber-100 border border-gray-800 rounded-md p-2 focus:outline-none focus:ring focus:border-amber-100 w-full"
+            />
+          </div>
+          <div className="flex flex-col items-center">
+            <label className="text-center">Email</label>
+            <input
+              type="email"
+              name="user_email"
+              className=" bg-amber-100 border border-gray-800 rounded-md p-2 focus:outline-none focus:ring focus:border-amber-100 w-full"
+            />
+          </div>
+          <div className="flex flex-col items-center">
+            <label className="text-center">Message</label>
+            <textarea
+              name="message"
+              className="bg-amber-100 border border-gray-800 rounded-md p-2 focus:outline-none focus:ring focus:border-amber-100 w-full"
+            />
+          </div>
+          <div className="flex justify-center">
+            <input
+              type="submit"
+              value="Send"
+              className="bg-indigo-800 text-grey-800 px-4 py-2 rounded"
+            />
+          </div>
+        </form>
       </div>
     </div>
-  );
+  </div>
+);
 };
 
 export default ContactUs; // Export the component properly
